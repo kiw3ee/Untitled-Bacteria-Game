90dfb35b5279b84285786808ee80fb38299ccb8b
kiw3ee <kwohanian@gmail.com>
2025-04-27 16:46:20 -0400

    added health system

---
+++ Lab3FR/Assets/Scripts/CollisionHealthDecrease.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHealthDecrease : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.tag == "Player")
+        {
+            var healthComponent = collision.GetComponent<HealthScript>();
+            if (healthComponent != null)
+            {
+                healthComponent.TakeDamage(1);
+            }
+        }
+    }
+}
---
+++ Lab3FR/Assets/Scripts/HealthScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthScript : MonoBehaviour
+{
+    public int maxHealth = 3;
+    public int currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+
+        if (currentHealth <= 0)
+        {
+            Debug.Log("You have died.");
+            Time.timeScale = 0f;
+        }
+    }
+}
