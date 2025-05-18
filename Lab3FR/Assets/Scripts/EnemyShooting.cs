927567259291bead8f8803f846f8884659f26ba1
MK3305 <michaelagrace33@gmail.com>
2025-05-18 14:26:00 -0400

    Shooting Enemies (w a bug tho)

---
+++ Lab3FR/Assets/Scripts/EnemyShooting.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooting : MonoBehaviour
+{
+    public Transform player;
+    public Transform enemyBulletSpawnPoint;
+    public GameObject enemyBulletPrefab;
+
+    public float detectionRadius = 10f;
+    public float bulletSpeed = 10f;
+    public float fireRate = 2f;
+
+    private float nextFireTime = 0f;
+
+    void Update()
+    {   
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        if (distanceToPlayer <= detectionRadius && Time.time >= nextFireTime)
+        {
+            var bullet = Instantiate(enemyBulletPrefab, enemyBulletSpawnPoint.position, enemyBulletSpawnPoint.rotation);
+            bullet.GetComponent<Rigidbody>().velocity = enemyBulletSpawnPoint.forward * bulletSpeed;
+
+            nextFireTime = Time.time + fireRate;
+        }
+    }
+}
